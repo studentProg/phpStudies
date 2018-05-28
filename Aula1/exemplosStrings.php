9df6568664bd94bab75c399f6d3c94d2c761f71c
root <root@localhost.localdomain>
2018-05-28 09:31:15 -0300

    commit Inicial

---
+++ Aula1/exemplosStrings.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+                $nome = "Treinamento hcode";
+                $a = $_GET['var'];
+                echo $a;
+                echo "<br>"."$a"."<br>";
+                echo strtoupper($nome)."<br>";
+                var_dump($a)."<br>";
+                echo "<br>".ucwords($nome);
+                
+                $nome = str_replace("hcode", "dudz", $nome);
+                echo "<br>".$nome;
+                $arrayString = ["parentes","mae",$nome];
+                echo "<br>".$arrayString[1];
+        ?>
+    </body>
+</html>
---
+++ html/teste.php
@@ -0,0 +1,6 @@
+array (
+  'id' => '../var/www/html/teste.php',
+  'perda' => 0,
+  'compra' => 0,
+  'seila' => '<?php echo "teste"; ?>',
+)
