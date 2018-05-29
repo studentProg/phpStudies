02bde3adbed5b7276689b4d32cd4029c443b2835
studentprog <studentprog@git.com>
2018-05-29 09:29:11 -0300

    adicionando mais aulas

--- Aula1/for.php
+++ Aula1/for.php
@@ -9,15 +9,38 @@ if
     </head>
     <body>
         <?php
-            for ($i=0;$i<=50;$i+=5){
-                
-                if ($i>9 && $i<35){
-                    continue;
-                }else if ($i == 45)break;
-                echo $i . "<br>";
-                
-            }
+        $meses = ["Janeiro","Fevereiro","Março",
+                  "Abril","Maio","Junho",
+                  "Julho","Agosto","Setembro",
+                  "Outubro","Novembro","Dezembro"];
+        foreach ($meses as $key => $mes) {
+            echo $key+1 . " --> " . $mes ."<br>";
+        }
+        ?>
         
+        
+        
+        <form action="for.php" method="GET">
+            <input type="text" name="nome">
+            <input type="date" name="nascimento">
+            <input type="submit" value="OK">
+        </form>
+        
+        <?php
+            foreach($_GET as $indice => $valor){
+                echo "Nome do campo " . $indice;
+                echo "<br> Valor do campo: " . $valor ."<br><br>";
+            }
+            
+            $obj = new StdClass;
+            $obj-> foo = 'bar';
+            var_dump($obj);
+            
+            $pessoas = array();
+            array_push($pessoas, ["nome"=>"Eduardo","sobrenome"=>"seabra"]);
+            array_push($pessoas, ["nome"=>"Bruna","sobrenome"=>"Soares Seabra"]);
+            echo "<br>";
+            echo $pessoas[0]["nome"];
         ?>
     </body>
 </html>
